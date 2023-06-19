d76e7f216df37417cf3059286c35123f59c0eab8
Christian Jay F. Rosales <gargoth13@gmail.com>
2023-06-20 01:48:59 +0800

    fix: Fixed RockCard styling

--- src/components/rockcard.tsx
+++ src/components/rockcard.tsx
@@ -10,9 +10,9 @@ export interface rock {
 export default function RockCard(props: rock) {
     return (
         <motion.div className="card fs-5" initial={{ x: -100, opacity: 0 }} whileInView={{ x: 0, opacity: 100 }} viewport={{ once: true }} transition={{ duration: 1 }}>
-            <div className="row g-0 overflow-hidden" style={{ maxHeight: "30vh" }}>
-                <div className="col-md-2 overflow-hidden">
-                    <img className="img-thumbnail rounded-start" src={props.imageurl} />
+            <div className="row g-0 overflow-hidden">
+                <div className="col-md-2 overflow-hidden d-flex justify-content-center">
+                    <img className="img-thumbnail rounded-start" src={props.imageurl}  style={{ maxHeight: "30vh" }}/>
                 </div>
                 <div className="col-md-8 card-body">
                     <h5 className="card-title">{props.name}</h5>
