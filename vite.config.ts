c83d5a40595a0b2bae741ea68b0e3bf124985655
Christian Jay F. Rosales <gargoth13@gmail.com>
2023-06-19 23:59:03 +0800

    chore: Capitalization of page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/geol-zambales/",
+    base: "/Geol-Zambales/",
     plugins: [react()],
 })
